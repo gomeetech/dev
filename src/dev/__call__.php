2400641a0916faaee74bca5652360aea4bac267a
Doan LN <developer.vcc@gmail.com>
2022-04-06 16:29:13 +0700

    update

--- src/dev/__call__.php
+++ src/dev/__call__.php
@@ -18,28 +18,35 @@ function __call__($args = [])
             $param = $a;
         }
 
-        if(is_callable($f)){
+        if (is_callable($f)) {
             return call_user_func_array($f, $param);
-        }else{
-            switch($f){
+        } else {
+            switch ($f) {
                 case "make":
-                case 'create': 
-                    if(isset($param[0])){
+                case 'create':
+                    if (isset($param[0])) {
                         $t = array_shift($param);
                         $p = get_args_params($param);
                         $args = array_merge([$p['params']], $p['args']);
-                        if($t == 'table'){
+                        if ($t == 'table') {
                             call_user_func_array("create_{$t}", $args);
-                        }
-                        elseif(in_array(strtolower($t), ['service', 'provider', 'serviceproviders'])){
+                        } elseif (in_array(strtolower($t), ['service', 'provider', 'serviceproviders'])) {
                             call_user_func_array("create_provider", $args);
                         }
-                        
                     }
                     break;
 
+                case 'alter':
+                    if (isset($param[0])) {
+                        $t = array_shift($param);
+                        $p = get_args_params($param);
+                        $args = array_merge([$p['params']], $p['args']);
+                        if ($t == 'table') {
+                            call_user_func_array("alter_{$t}", $args);
+                        } 
+                    }
+                    break;
             }
         }
     }
 }
-
